c9723b0cb6c6e2a241d8882b1a189e6252b88b2b
JaveriaNigar <nigarhaider86@gmail.com>
2024-10-03 22:23:31 +0500

    Add files via upload

---
+++ ifelse/ifelse.js
@@ -0,0 +1,24 @@
+var isrishtaaccepted = true;
+if (isrishtaaccepted) {
+    console.log("eidi dedo");
+}
+else {
+    console.log("moye moye");
+}
+// else if 
+var game = "";
+game = "PubG";
+game = "freefire";
+game = "Angry Bird";
+if (game == "PubG") {
+    console.log("Winner Winner Chicken Dinner");
+}
+else if (game == "freefire") {
+    console.log("Congratulation You Win!");
+}
+else if (game == "Angry Bird") {
+    console.log("you win!");
+}
+else {
+    console.log("Not a game");
+}
---
+++ ifelse/ifelse.ts
@@ -0,0 +1,26 @@
+let isrishtaaccepted = true;
+
+if (isrishtaaccepted){
+    console.log("eidi dedo");
+
+} else {
+    console.log("moye moye");
+
+}
+
+// else if 
+
+let game: string = "Angry Bird";
+game = "PubG"
+game = "freefire"
+game = "Angry Bird"
+
+if(game == "PubG"){
+    console.log("Winner Winner Chicken Dinner");
+} else if(game == "freefire"){
+   console.log("Congratulation You Win!");
+} else if(game == "Angry Bird"){
+   console.log("you win!");
+} else{
+    console.log("Not a game");
+}
